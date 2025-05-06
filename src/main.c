3e9202708f7b7181a3028b9494d049f0ca6facd5
shashank-github-0001 <shashankbharadwaj0001@gmail.com>
2025-05-06 11:58:25 +0530

    Tue May  6 11:58:25 AM IST 2025

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 #include "../include/font.h"
 #include "../include/tasktree.h"
 
-// hello world
+// hello
 const char *MAIN_WINDOW_TITLE = "tt";
 const int32_t SCROLL_MULTIPLYER = 50;
 static const int32_t MAIN_WINDOW_WIDTH = 800;
